31ee0886be37bc029b67947fb986a8dd2386c8ed
netjose23 <jalvarado@italvaradoservices.com>
2021-06-11 16:14:14 -0700

    Update site.py

---
+++ ssg/site.py
@@ -0,0 +1,22 @@
+from os import replace
+from pathlib import Path
+
+
+class Site:
+
+    def __init__(self, source, dest):
+        source = Path()
+        dest = Path()
+
+    def create_dir(self, path):
+        directory = Path('C:\\pythoncode\\Pro-StaticSite\\python-static-site-generator\\ssg')
+        path.relative_to(self.source)
+        
+    def mkdir(directory, *args):
+        parents = True
+        exist_ok = True
+
+    def build()
+
+
+
