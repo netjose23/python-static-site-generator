d21e25a237021f287fbb423a954cb7c59c909c1a
netjose23 <jalvarado@italvaradoservices.com>
2021-06-14 13:29:40 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -5,15 +5,15 @@ from pathlib import Path
 class Site:
 
     def __init__(self, source, dest):
-        source = Path(source)
-        dest = Path(dest)
+        self.sourcesource = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.mkdir(parents=True, exists_okay=True)
+        directory.mkdir(parents=True, exist_okay=True)
     
     def build(self):
-        self.dest.mkdir(parents=True, exists_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if path is self.directory:
+            if path.is_dir():
                 self.create_dir(path)
